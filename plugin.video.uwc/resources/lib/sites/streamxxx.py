42b1e880afff1d12db180174047e2bedf5d1bda1
12asdfg12 <48175015+12asdfg12@users.noreply.github.com>
2019-12-14 11:31:54 +0200

    streamxxx & animeidhentai

--- plugin.video.uwc/resources/lib/sites/streamxxx.py
+++ plugin.video.uwc/resources/lib/sites/streamxxx.py
@@ -22,24 +22,20 @@ import re
 import xbmcplugin
 from resources.lib import utils
 
-progress = utils.progress
-
 
 @utils.url_dispatcher.register('170')
 def Main():
-    utils.addDir('[COLOR hotpink]Categories[/COLOR]','http://streamxxx.tv/', 177, '', '')
+#    utils.addDir('[COLOR hotpink]Categories[/COLOR]','http://streamxxx.tv/', 177, '', '')
     utils.addDir('[COLOR hotpink]Tags[/COLOR]','http://streamxxx.tv/top-tags/', 173, '', '')
     utils.addDir('[COLOR hotpink]Movies[/COLOR]','http://streamxxx.tv/category/movies-xxx/', 175, '', '')
     utils.addDir('[COLOR hotpink]International Movies[/COLOR]','http://streamxxx.tv/category/movies-xxx/international-movies/', 176, '', '')
     utils.addDir('[COLOR hotpink]Search Overall[/COLOR]','http://streamxxx.tv/?s=', 174, '', '')
     utils.addDir('[COLOR hotpink]Search Scenes[/COLOR]','http://streamxxx.tv/?cat=5562&s=', 174, '', '')
     List('http://streamxxx.tv/category/clips/')
-    xbmcplugin.endOfDirectory(utils.addon_handle)
 
 
 @utils.url_dispatcher.register('175')
 def MainMovies():
-
     List('http://streamxxx.tv/category/movies-xxx/')
     xbmcplugin.endOfDirectory(utils.addon_handle)
 
@@ -55,17 +51,16 @@ def List(url):
     try:
         listhtml = utils.getHtml(url, url)
     except:
-        
         return None
-    match = re.compile('<h2 class="st-loop-entry-title">.*?<a href="([^"]+)".*?title="([^"]+)".*?src="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    listhtml = listhtml.split('>Next<')[0] + '>Next<'
+    match = re.compile('href=(https://streamxxx\S+)\s+title="([^"]+)".+?img src=(\S+)\s', re.DOTALL | re.IGNORECASE).findall(listhtml)
     for videopage, name, img in match:
         name = utils.cleantext(name)
-        if videopage.startswith('/'):
-            videopage = 'http://streamxxx.tv/' + videopage
         utils.addDownLink(name, videopage, 172, img, '')
     try:
-        nextp=re.compile('<a class="next page-numbers" href="([^"]+)">Next', re.DOTALL | re.IGNORECASE).findall(listhtml)[0]
-        utils.addDir('Next Page', nextp, 171,'')
+        next_page=re.compile('class="next page-numbers" href=([\S]+)\s*>Next', re.DOTALL | re.IGNORECASE).findall(listhtml)[0]
+        page_nr = re.findall('\d+', next_page)[-1]
+        utils.addDir('Next Page (' + page_nr + ')', next_page, 171,'')
     except: pass
     xbmcplugin.endOfDirectory(utils.addon_handle)
 
@@ -95,13 +90,25 @@ def Categories(url):
 
 @utils.url_dispatcher.register('173', ['url'])
 def Tags(url):
-	html = utils.getHtml(url, '')
-	match1 = re.compile('<a id="tag-link.+?href="([^"]+)".+?"tag">(.+?)</a>', re.DOTALL | re.IGNORECASE).findall(html)
-	for catpage, name in match1:
-		utils.addDir(name, catpage, 171, '')
-	xbmcplugin.endOfDirectory(utils.addon_handle)
+    html = utils.getHtml(url, '')
+    html = html.replace('><','')
+    match1 = re.compile('href=(\S+tv/tag/\S+)\s[^>]+>([^<]+)<', re.DOTALL | re.IGNORECASE).findall(html)
+    for catpage, name in match1:
+        utils.addDir(name, catpage, 171, '')
+    xbmcplugin.endOfDirectory(utils.addon_handle)
 
 
 @utils.url_dispatcher.register('172', ['url', 'name'], ['download'])
 def Playvid(url, name, download=None):
-    utils.PLAYVIDEO(url, name, download)
+    vp = utils.VideoPlayer(name, download)
+    vp.progress.update(25, "", "Loading video page", "")
+    videopage = utils.getHtml(url)
+    videourls = re.compile('a href=(\S+)\s[^>]+>[^<]+</a>', re.DOTALL | re.IGNORECASE).findall(videopage)
+    links = {}
+    for link in videourls:
+        if vp.resolveurl.HostedMediaFile(link):
+            links[link.split('/')[2]] = link
+    videourl = utils.selector('Select link', links, dont_ask_valid=False)
+    vp.play_from_link_to_resolve(videourl)
+
+
