7eec25065818c5bae3b5f4f2bb18daa832d787e4
12asdfg12 <48175015+12asdfg12@users.noreply.github.com>
2020-02-29 20:22:27 +0200

    eporner, hclips

--- plugin.video.uwc/resources/lib/sites/eporner.py
+++ plugin.video.uwc/resources/lib/sites/eporner.py
@@ -25,11 +25,11 @@ siteurl = 'https://www.eporner.com'
     
 @utils.url_dispatcher.register('540')
 def Main():
-    utils.addDir('[COLOR hotpink]Categories[/COLOR]',siteurl + '/categories/',543,'','')
+    utils.addDir('[COLOR hotpink]Categories[/COLOR]',siteurl + '/cats/',543,'','')
     utils.addDir('[COLOR hotpink]Pornstars[/COLOR]',siteurl + '/pornstars/',545,'','')
     utils.addDir('[COLOR hotpink]Lists[/COLOR]',siteurl,546,'','')
     utils.addDir('[COLOR hotpink]Search[/COLOR]',siteurl+'/search/',544,'','')
-    List(siteurl + '/category/all/')
+    List(siteurl + '/cat/all/')
     xbmcplugin.endOfDirectory(utils.addon_handle)
 
 @utils.url_dispatcher.register('541', ['url'])
@@ -38,10 +38,15 @@ def List(url):
         listhtml = utils.getHtml(url, '')
     except:
         return None
-    match = re.compile('data-vpid.+?><span>([^<]+)<.+?href="([^"]+)".+?img src="([^"]+)".+?title="([^"]+)">.+?title="Duration">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for hd, videopage, img, name, duration in match:
-        name = utils.cleantext(name) + "[COLOR orange] " + hd + "[COLOR deeppink] " +  duration + "[/COLOR]"
-        utils.addDownLink(name, siteurl + videopage, 542, img, '')
+    vids = listhtml.split('data-vpid=')
+    for i, vid in enumerate(vids):
+        if i == 0:
+            continue
+        match = re.compile('<span>([^<]+)<.+?href="([^"]+)".+?src="(http[^"]+)".+?title="([^"]+)">.+?title="Duration">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(vid)
+        if match:
+            hd, videopage, img, name, duration = match[0]
+            name = utils.cleantext(name) + "[COLOR orange] " + hd + "[COLOR deeppink] " +  duration + "[/COLOR]"
+            utils.addDownLink(name, siteurl + videopage, 542, img, '')
     try:
         nextp, page = re.compile("href='([^']+/(\d+)/)' class='nmnext' title='Next page'", re.DOTALL | re.IGNORECASE).findall(listhtml)[0]
         utils.addDir('Next Page (' + page +')', siteurl + nextp, 541,'')
@@ -87,30 +92,30 @@ def Pornstars(url):
 @utils.url_dispatcher.register('546', ['url'])
 def Lists(url):
     lists = {}
-    lists['HD Porn 1080p Videos - Recent'] = siteurl + '/category/hd-1080p/'
-    lists['HD Porn 1080p Videos - Top Rated'] = siteurl + '/category/hd-1080p/SORT-top-rated/'
-    lists['HD Porn 1080p Videos - Longest'] = siteurl + '/category/hd-1080p/SORT-longest/'
-    lists['60 FPS Porn Videos - Recent'] = siteurl + '/category/60fps/'
-    lists['60 FPS Porn Videos - Top Rated'] = siteurl + '/category/60fps/SORT-top-rated/'
-    lists['60 FPS Porn Videos - Longest'] = siteurl + '/category/60fps/SORT-longest/'
+    lists['HD Porn 1080p Videos - Recent'] = siteurl + '/cat/hd-1080p/'
+    lists['HD Porn 1080p Videos - Top Rated'] = siteurl + '/cat/hd-1080p/SORT-top-rated/'
+    lists['HD Porn 1080p Videos - Longest'] = siteurl + '/cat/hd-1080p/SORT-longest/'
+    lists['60 FPS Porn Videos - Recent'] = siteurl + '/cat/60fps/'
+    lists['60 FPS Porn Videos - Top Rated'] = siteurl + '/cat/60fps/SORT-top-rated/'
+    lists['60 FPS Porn Videos - Longest'] = siteurl + '/cat/60fps/SORT-longest/'
     lists['Popular Porn Videos'] = siteurl + '/popular-videos/'
     lists['Best HD Porn Videos'] = siteurl + '/top-rated/'
     lists['Currently Watched Porn Videos'] = siteurl + '/currently/'
-    lists['4K Porn Ultra HD - Recent'] = siteurl + '/category/4k-porn/'
-    lists['4K Porn Ultra HD - Top Rated'] = siteurl + '/category/4k-porn/SORT-top-rated/'
-    lists['4K Porn Ultra HD - Longest'] = siteurl + '/category/4k-porn/SORT-longest/'
-    lists['HD Sex Porn Videos - Recent'] = siteurl + '/category/hd-sex/'
-    lists['HD Sex Porn Videos - Top Rated'] = siteurl + '/category/hd-sex/SORT-top-rated/'
-    lists['HD Sex Porn Videos - Longest'] = siteurl + '/category/hd-sex/SORT-longest/'
-    lists['Amateur Porn Videos - Recent'] = siteurl + '/category/amateur/'
-    lists['Amateur Porn Videos - Top Rated'] = siteurl + '/category/amateur/SORT-top-rated/'
-    lists['Amateur Porn Videos - Longest'] = siteurl + '/category/amateur/SORT-longest/'
-    lists['Solo Girls Porn Videos - Recent'] = siteurl + '/category/solo/'
-    lists['Solo Girls Porn Videos - Top Rated'] = siteurl + '/category/solo/top-rated/'
-    lists['Solo Girls Porn Videos - Longest'] = siteurl + '/category/solo/longest/'
-    lists['VR Porn Videos - Recent'] = siteurl + '/category/vr-porn/'
-    lists['VR Porn Videos - Top Rated'] = siteurl + '/category/vr-porn/SORT-top-rated/'
-    lists['VR Porn Videos - Longest'] = siteurl + '/category/vr-porn/SORT-longest/'
+    lists['4K Porn Ultra HD - Recent'] = siteurl + '/cat/4k-porn/'
+    lists['4K Porn Ultra HD - Top Rated'] = siteurl + '/cat/4k-porn/SORT-top-rated/'
+    lists['4K Porn Ultra HD - Longest'] = siteurl + '/cat/4k-porn/SORT-longest/'
+    lists['HD Sex Porn Videos - Recent'] = siteurl + '/cat/hd-sex/'
+    lists['HD Sex Porn Videos - Top Rated'] = siteurl + '/cat/hd-sex/SORT-top-rated/'
+    lists['HD Sex Porn Videos - Longest'] = siteurl + '/cat/hd-sex/SORT-longest/'
+    lists['Amateur Porn Videos - Recent'] = siteurl + '/cat/amateur/'
+    lists['Amateur Porn Videos - Top Rated'] = siteurl + '/cat/amateur/SORT-top-rated/'
+    lists['Amateur Porn Videos - Longest'] = siteurl + '/cat/amateur/SORT-longest/'
+    lists['Solo Girls Porn Videos - Recent'] = siteurl + '/cat/solo/'
+    lists['Solo Girls Porn Videos - Top Rated'] = siteurl + '/cat/solo/top-rated/'
+    lists['Solo Girls Porn Videos - Longest'] = siteurl + '/cat/solo/longest/'
+    lists['VR Porn Videos - Recent'] = siteurl + '/cat/vr-porn/'
+    lists['VR Porn Videos - Top Rated'] = siteurl + '/cat/vr-porn/SORT-top-rated/'
+    lists['VR Porn Videos - Longest'] = siteurl + '/cat/vr-porn/SORT-longest/'
     url = utils.selector('Select', lists)
     if not url:
         return
