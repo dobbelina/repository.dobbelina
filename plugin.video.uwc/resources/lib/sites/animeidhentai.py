8d8d70d2a4703a3db3601f30385cde95520fba96
dobbelina <46063764+dobbelina@users.noreply.github.com>
2020-04-29 09:49:18 +0200

    Update animeidhentai.py

--- plugin.video.uwc/resources/lib/sites/animeidhentai.py
+++ plugin.video.uwc/resources/lib/sites/animeidhentai.py
@@ -24,7 +24,7 @@ from resources.lib import utils
 
 @utils.url_dispatcher.register('660')
 def animeidhentai_main():
-    utils.addDir('[COLOR hotpink]Uncensored[/COLOR]','https://animeidhentai.com/genre/hentai-uncensored/', 661, '', '')
+    utils.addDir('[COLOR hotpink]Uncensored[/COLOR]','https://animeidhentai.com/hentai/uncensored-hentai/', 661, '', '')
     utils.addDir('[COLOR hotpink]3D[/COLOR]','https://animeidhentai.com/genre/3d/', 661, '', '')
     utils.addDir('[COLOR hotpink]Search[/COLOR]','https://animeidhentai.com/?s=', 664, '', '')
     animeidhentai_list('https://animeidhentai.com/genre/2020/')
