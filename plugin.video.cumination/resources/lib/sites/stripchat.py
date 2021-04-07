96470488d6f40cbbd40b35b19b67f035fc5cfe73
dobbelina <46063764+dobbelina@users.noreply.github.com>
2021-04-07 21:26:24 +0200

    Added Stripchat webcams
    And Bongacams update

--- plugin.video.cumination/resources/lib/sites/stripchat.py
+++ plugin.video.cumination/resources/lib/sites/stripchat.py
@@ -22,7 +22,7 @@ import requests
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('bongacams', '[COLOR hotpink]bongacams.com[/COLOR]', 'http://bongacams.com', 'bongacams.png', 'bongacams', True)
+site = AdultSite('stripchat', '[COLOR hotpink]stripchat.com[/COLOR]', 'http://stripchat.com', 'stripchat.jpg', 'stripchat', True)
 
 
 @site.register(default_mode=True)
@@ -31,21 +31,18 @@ def Main():
     male = True if utils.addon.getSetting("chatmale") == "true" else False
     couple = True if utils.addon.getSetting("chatcouple") == "true" else False
     trans = True if utils.addon.getSetting("chattrans") == "true" else False
-    site.add_dir('[COLOR red]Refresh bongacams.com images[/COLOR]', '', 'clean_database', '', Folder=False)
+    site.add_dir('[COLOR red]Refresh Stripchat images[/COLOR]', '', 'clean_database', '', Folder=False)
 
-    bu = "http://tools.bongacams.com/promo.php?c=226355&type=api&api_type=json&categories[]="
+    bu = "https://stripchat.com/api/external/v4/widget/?limit=1000&modelsCountry=&modelsLanguage=&modelsList=&tag="
+    site.add_dir('[COLOR hotpink]HD[/COLOR]', '{0}hd'.format(bu), 'List', '', '')
     if female:
         site.add_dir('[COLOR hotpink]Female[/COLOR]', '{0}female'.format(bu), 'List', '', '')
     if couple:
         site.add_dir('[COLOR hotpink]Couples[/COLOR]', '{0}couples'.format(bu), 'List', '', '')
     if male:
-        site.add_dir('[COLOR hotpink]Male[/COLOR]', '{0}male'.format(bu), 'List', '', '')
+        site.add_dir('[COLOR hotpink]Male[/COLOR]', '{0}men'.format(bu), 'List', '', '')
     if trans:
-        site.add_dir('[COLOR hotpink]Transsexual[/COLOR]', '{0}transsexual'.format(bu), 'List', '', '')
-    site.add_dir('Hour\'s TOP chat rooms', 'https://bongacams.com/ajax-top-room-contest', 'List2', '', '')
-    site.add_dir('New North America & Western Europe\'s contest - Queen of Queens', 'https://bongacams.com/ajax-queen-of-queens-contest', 'List3', '', '')
-    site.add_dir('New international contest - Queen of Queens', 'https://bongacams.com/ajax-queen-of-queens-contest-international', 'List3', '', '')
-
+        site.add_dir('[COLOR hotpink]Transsexual[/COLOR]', '{0}trans'.format(bu), 'List', '', '')
     utils.eod()
 
 
@@ -54,44 +51,20 @@ def List(url):
     if utils.addon.getSetting("chaturbate") == "true":
         clean_database(False)
 
-    data = utils._getHtml(url)
-    model_list = json.loads(data)
+    try:
+        response = utils.getHtml(url)
+    except:
+        return None
+    data = json.loads(response)
+    model_list = data["models"]
+
     for model in model_list:
-        img = 'https:' + model['profile_images']['thumbnail_image_big_live']
-        username = model['username']
-        name = model['display_name']
-        subject = name
-        age = model['display_age']
-        name += ' [COLOR hotpink][{}][/COLOR]'.format(age)
-        if model['hd_cam']:
-            name += ' [COLOR gold]HD[/COLOR]'
-        subject += u', {}'.format(age)
-        subject += u', {}\n'.format(model['hometown']) if model['hometown'] else u'\n'
-        if model['ethnicity']:
-            subject += u'- {}\n'.format(model['ethnicity'])
-        if model['primary_language']:
-            subject += u'- Speaks {}\n'.format(model['primary_language'])
-        if model['secondary_language']:
-            subject = subject[:-1] + u', {}\n'.format(model['secondary_language'])
-        if model['eye_color']:
-            subject += u'- {} Eyed\n'.format(model['eye_color'])
-        if model['hair_color']:
-            subject = subject[:-1] + u' {}\n'.format(model['hair_color'])
-        if model['height']:
-            subject += u'- {} tall\n'.format(model['height'])
-        if model['weight']:
-            subject += u'- {} weight\n'.format(model['weight'])
-        if model['bust_penis_size']:
-            subject += u'- {} Boobs\n'.format(model['bust_penis_size']) if 'Female' in model['gender'] else u'- {} Cock\n'.format(model['bust_penis_size'])
-        if model['pubic_hair']:
-            subject = subject[:-1] + u' and {} Pubes\n'.format(model['pubic_hair'])
-        if model['vibratoy']:
-            subject += u'- Lovense Toy\n'
-        if model['turns_on']:
-            subject += u'- Likes: {}\n'.format(model['turns_on'])
-        if model['turns_off']:
-            subject += u'- Dislikes: {}\n'.format(model['turns_off'])
-        site.add_download_link(name, username, 'Playvid', img, subject, noDownload=True)
+        name = utils.cleanhtml(model['username']).encode("ascii", errors="ignore")
+        videourl = model['stream']['url']
+        img = model['previewUrl'] if utils.addon.getSetting(
+            "model_pic") == "0" else model['snapshotUrl']
+
+        site.add_download_link(name, videourl, 'Playvid', img, '', noDownload=True)
     utils.eod()
 
 
@@ -100,16 +73,16 @@ def clean_database(showdialog=True):
     conn = sqlite3.connect(utils.TRANSLATEPATH("special://database/Textures13.db"))
     try:
         with conn:
-            list = conn.execute("SELECT id, cachedurl FROM texture WHERE url LIKE '%%%s%%';" % "bongacams.com")
+            list = conn.execute("SELECT id, cachedurl FROM texture WHERE url LIKE '%%%s%%';" % ".stripst.com")
             for row in list:
                 conn.execute("DELETE FROM sizes WHERE idtexture LIKE '%s';" % row[0])
                 try:
                     os.remove(utils.TRANSLATEPATH("special://thumbnails/" + row[1]))
                 except:
                     pass
-            conn.execute("DELETE FROM texture WHERE url LIKE '%%%s%%';" % "bongacams.com")
+            conn.execute("DELETE FROM texture WHERE url LIKE '%%%s%%';" % ".stripst.com")
             if showdialog:
-                utils.notify('Finished', 'bongacams.com images cleared')
+                utils.notify('Finished', 'Stripchat images cleared')
     except:
         pass
 
@@ -118,37 +91,18 @@ def clean_database(showdialog=True):
 def Playvid(url, name):
     vp = utils.VideoPlayer(name)
     vp.progress.update(25, "[CR]Loading video page[CR]")
-    try:
-        postRequest = {'method': 'getRoomData', 'args[]': str(url)}
-        response = utils._postHtml('{0}/tools/amf.php'.format(site.url), form_data=postRequest, headers={'X-Requested-With': 'XMLHttpRequest'}, compression=False)
-    except:
-        utils.notify('Oh oh', 'Couldn\'t find a playable webcam link')
-        return None
+    altUrl = 'https://stripchat.com/api/external/v4/widget/?limit=1&modelsList='
+    if requests.get(url).status_code != 200:
+        data = json.loads(utils.getHtml(altUrl + name))["models"][0]
+        if data["username"] == name:
+            url = data['stream']['url']
+        else:
+            xbmc.executebuiltin("Notification(" + name + ",Couldn\'t find a playable webcam link, ," + xbmc.getInfoImage("ListItem.Thumb") + ")")
+            return
 
-    amf_json = json.loads(response)
-    try:
-        amf = amf_json['localData']['videoServerUrl']
-    except:
-        xbmcgui.Dialog().notification(name, 'Couldn\'t find a playable webcam link', icon=xbmc.getInfoImage("ListItem.Thumb"))
-        return
-
-    if amf_json['localData']['videoServerUrl'].startswith("//mobile"):
-        videourl = 'https:' + amf_json['localData']['videoServerUrl'] + '/hls/stream_' + url + '.m3u8'
-    else:
-        videourl = 'https:' + amf_json['localData']['videoServerUrl'] + '/hls/stream_' + url + '/playlist.m3u8'
-
-    response = requests.get(videourl)
-    if response.status_code !=200:
-        xbmcgui.Dialog().notification(name, 'Couldn\'t find a playable webcam link', icon=xbmc.getInfoImage("ListItem.Thumb"))
-        return
-    if response.status_code == 200 and '#EXT-X-STREAM-INF' not in response.text:
-        xbmcgui.Dialog().notification(name, 'In private chat', icon=xbmc.getInfoImage("ListItem.Thumb"))
-        return
-
-    videourl += '|User-Agent={0}'.format(utils.USER_AGENT)
     vp.progress.update(75, "[CR]Found Stream[CR]")
     vp = utils.VideoPlayer(name)
-    vp.play_from_direct_link(videourl)
+    vp.play_from_direct_link(url)
 
 
 @site.register()
