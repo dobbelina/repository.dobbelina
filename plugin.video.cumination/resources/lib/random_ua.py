67e68ebb54228631ac6c36104220600835191147
dobbelina <46063764+dobbelina@users.noreply.github.com>
2022-08-09 08:18:28 +0200

    remove old version from random ua generation #830

--- plugin.video.cumination/resources/lib/random_ua.py
+++ plugin.video.cumination/resources/lib/random_ua.py
@@ -17,20 +17,20 @@ def set_setting(id, value):
 
 
 BR_VERS = [
-    ['%s.0' % i for i in range(60, 90)],
-    ['48.0.2564.109', '49.0.2623.75', '50.0.2661.75', '51.0.2704.84', '53.0.2785.116', '54.0.2840.71', '55.0.2883.75', '56.0.2924.87',
-     '57.0.2987.133', '58.0.3029.96', '59.0.3071.86', '60.0.3112.78', '61.0.3163.79', '62.0.3202.75', '63.0.3239.108', '64.0.3282.140',
-     '65.0.3325.181', '66.0.3359.181', '67.0.3396.79', '68.0.3440.84', '69.0.3497.92', '70.0.3538.77', '71.0.3578.80', '75.0.3770.80',
-     '76.0.3809.100', '78.0.3904.97', '79.0.3945.88', '80.0.3987.149', '81.0.4044.92', '83.0.4103.116', '84.0.4147.135', '86.0.4240.75',
-     '90.0.4430.72', '91.0.4472.147'],
-    ['11.0'],
-    ['10.0', '10.6']]
-WIN_VERS = ['Windows NT 10.0', 'Windows NT 7.0', 'Windows NT 6.3', 'Windows NT 6.2']  # , 'Windows NT 6.1', 'Windows NT 6.0', 'Windows NT 5.1', 'Windows NT 5.0']
+    ['%s.0' % i for i in range(80, 103)],
+    ['75.0.3770.80', '76.0.3809.100', '78.0.3904.97', '79.0.3945.88', '80.0.3987.149',
+     '81.0.4044.92', '83.0.4103.116', '84.0.4147.135', '86.0.4240.75', '90.0.4430.72',
+     '91.0.4472.147', '101.0.0.0', '102.0.0.0', '106.0.0.0'],
+    ['84.0.4147.135', '86.0.4240.75', '90.0.4430.72',
+     '91.0.4472.147', '101.0.0.0', '102.0.0.0', '106.0.0.0']
+]
+WIN_VERS = ['Windows NT 10.0', 'Windows NT 7.0', 'Windows NT 6.3', 'Windows NT 6.2']
 FEATURES = ['; WOW64', '; Win64; x64', '']
-RAND_UAS = ['Mozilla/5.0 ({win_ver}{feature}; rv:{br_ver}) Gecko/20100101 Firefox/{br_ver}',
-            'Mozilla/5.0 ({win_ver}{feature}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{br_ver} Safari/537.36',
-            'Mozilla/5.0 ({win_ver}{feature}; Trident/7.0; rv:{br_ver}) like Gecko',
-            'Mozilla/5.0 (compatible; MSIE {br_ver}; {win_ver}{feature}; Trident/6.0)']
+RAND_UAS = [
+    'Mozilla/5.0 ({win_ver}{feature}; rv:{br_ver}) Gecko/20100101 Firefox/{br_ver}',
+    'Mozilla/5.0 ({win_ver}{feature}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{br_ver} Safari/537.36',
+    'Mozilla/5.0 ({win_ver}{feature}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{br_ver} Safari/537.36 Edg/{br_ver}'
+]
 
 
 def get_ua():
