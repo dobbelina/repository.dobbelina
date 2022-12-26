27228ba9ce95ccfae47a803ae4a0f085f9a7418a
dobbelina <46063764+dobbelina@users.noreply.github.com>
2022-12-26 19:18:23 +0100

    Chaturbate listing code revert

--- plugin.video.cumination/resources/lib/sites/chaturbate.py
+++ plugin.video.cumination/resources/lib/sites/chaturbate.py
@@ -116,10 +116,10 @@ def List(url, page=1):
         clean_database(False)
 
     listhtml = utils._getHtml(url)
-    match = re.compile(r'room_list_room".+?href="([^"]+).+?src="([^"]+).+?</a>.+?class="username">[^>]+>\s*([^<]+).+?age">([^<]+).+?subject">\s*(.+?)</div.+?inner">([^<]+).+?time">([^<]+).+?viewers">([^<]+)', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for videopage, img, name, age, subject, location, duration, viewers in match:
+    match = re.compile(r'room_list_room".+?href="([^"]+).+?src="([^"]+).+?</a>(.*?)<div class="details">.+?href[^>]+>([^<]+)<.+?age">([^<]+).+?subject">.+?>(.*?)</li.+?location.+?>([^<]+).+?time">([^<]+).+?viewers">([^<]+)', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for videopage, img, status, name, age, subject, location, duration, viewers in match:
         name = utils.cleantext(name)
-        age = age.replace('&nbsp;', '').strip()
+        age = age.replace('&nbsp;', '')
         tags = re.findall(r'>#([^<]+)', subject)
         subject = re.sub(r'<.+>', '', subject)
         subject = utils.cleantext(subject) + "[CR][CR][COLOR deeppink]Location: [/COLOR]" + utils.cleantext(location) + "[CR]" \
@@ -127,8 +127,11 @@ def List(url, page=1):
             + "[COLOR deeppink]Watching: [/COLOR]" + utils.cleantext(viewers)
         if tags:
             tags = '[COLOR deeppink]#[/COLOR]' + ', [COLOR deeppink]#[/COLOR]'.join(tags)
-            subject += "[CR][COLOR deeppink]Tags: [/COLOR]" + tags
-        name = name + " [COLOR deeppink][" + age + "][/COLOR]"
+            subject += "[CR][CR]" + tags
+        status = utils.cleantext(status.replace("[CR]", ""))
+        if status:
+            status = status.split('>')[1].split('<')[0]
+        name = name + " [COLOR deeppink][" + age + "][/COLOR] " + status
         videopage = bu[:-1] + videopage
         site.add_download_link(name, videopage, 'Playvid', img, subject, noDownload=True)
 
