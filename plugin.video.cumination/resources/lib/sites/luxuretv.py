19c01fd5501e390581f6dd9fe1bb2fdf2911b201
IdentDee <126449726+IdentDee@users.noreply.github.com>
2023-02-27 08:24:48 +0100

    Fix search for luxuretv.com

--- plugin.video.cumination/resources/lib/sites/luxuretv.py
+++ plugin.video.cumination/resources/lib/sites/luxuretv.py
@@ -60,7 +60,7 @@ def Search(url, keyword=None):
     if not keyword:
         site.search_dir(url, 'Search')
     else:
-        url = "{0}{1}/".format(url, keyword.replace(' ', '+'))
+        url = "{0}{1}/".format(url, keyword.replace(' ', '-'))
         List(url)
 
 
