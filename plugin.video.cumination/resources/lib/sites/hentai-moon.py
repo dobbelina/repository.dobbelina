6a50f4cd1068d5795d9bc51e859536ad084e05c3
dobbelina <46063764+dobbelina@users.noreply.github.com>
2023-05-23 11:06:37 +0000

    New sites: Hentai-Moon and SeaPorn

---
+++ plugin.video.cumination/resources/lib/sites/hentai-moon.py
@@ -0,0 +1,198 @@
+"""
+    Cumination
+    Copyright (C) 2023 Team Cumin   
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+import xbmc
+from six.moves import urllib_parse
+from resources.lib import utils
+from resources.lib.adultsite import AdultSite
+from resources.lib.decrypters.kvsplayer import kvs_decode
+
+site = AdultSite("hentai-moon", "[COLOR hotpink]Hentai Moon[/COLOR]", 'https://hentai-moon.com/', "hentai-moon.png", "hentai-moon")
+
+ajaxlist = '?mode=async&function=get_block&block_id=list_videos_latest_videos_list&sort_by=post_date&from=1'
+ajaxcommon = '?mode=async&function=get_block&block_id=list_videos_common_videos_list&sort_by=post_date&from=1'
+
+
+@site.register(default_mode=True)
+def Main():
+    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url + 'categories/?mode=async&function=get_block&block_id=list_categories_categories_list&sort_by=title', 'Categories', site.img_cat)
+    site.add_dir('[COLOR hotpink]Series[/COLOR]', site.url + 'series/?mode=async&function=get_block&block_id=list_dvds_channels_list&sort_by=title&from=1', 'Series', site.img_cat)
+    site.add_dir('[COLOR hotpink]Tags[/COLOR]', site.url + 'tags/', 'Tags', site.img_cat)
+    site.add_dir('[COLOR hotpink]Top Rated[/COLOR]', site.url + 'top-rated/' + ajaxlist, 'List', site.img_cat)
+    site.add_dir('[COLOR hotpink]Most Viewed[/COLOR]', site.url + 'most-popular/' + ajaxlist, 'List', site.img_cat)
+    site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + 'search/', 'Search', site.img_search)
+    List(site.url + 'latest-updates/' + ajaxlist)
+
+
+@site.register()
+def List(url):
+    listhtml = utils.getHtml(url, site.url)
+    match = re.compile(r'href="([^"]+)"\s+?title="([^"]+)".*?data-original="([^"]+)"[^>]+(.*?)</div>.*?duration">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for videopage, name, img, hd, duration in match:
+        name = utils.cleantext(name)
+        hd = " [COLOR orange]HD[/COLOR]" if 'is_hd' in hd else ''
+        contexturl = (utils.addon_sys
+                      + "?mode=" + str('hentai-moon.Lookupinfo')
+                      + "&url=" + urllib_parse.quote_plus(videopage))
+        contextmenu = ('[COLOR deeppink]Lookup info[/COLOR]', 'RunPlugin(' + contexturl + ')')
+        site.add_download_link(name, videopage, 'Playvid', img, name, contextm=contextmenu, duration=duration, quality=hd)
+    np = re.compile(r':(\d+)">Next', re.DOTALL | re.IGNORECASE).search(listhtml)
+    if np:
+        np = np.group(1)
+        pagelookup = re.search(r"(from(?:_videos)?)=(\d+)", url)
+        if pagelookup:
+            page = pagelookup.group(2)
+            fromtxt = pagelookup.group(1)
+            url = url.replace(f"{fromtxt}={page}", f"{fromtxt}={np}")
+            site.add_dir(f'Next Page ({np})', url, 'List', site.img_next)
+
+    utils.eod()
+
+
+@site.register()
+def Categories(url):
+    listhtml = utils.getHtml(url)
+    match = re.compile(r'item"\s+?href="([^"]+)"\s?title="([^"]+)".*?src="([^"]+)".*?videos">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for catpage, name, img, videos in match:
+        name = utils.cleantext(name) + " [COLOR deeppink]" + videos.strip() + "[/COLOR]"
+        catpage = catpage + ajaxcommon
+        site.add_dir(name, catpage, 'List', img, name)
+    utils.eod()
+
+
+@site.register()
+def Tags(url):
+    listhtml = utils.getHtml(url)
+    match = re.compile('/(tags/[^"]+)">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for tagpage, name in match:
+        name = utils.cleantext(name)
+        tagpage = site.url + tagpage + ajaxcommon
+        site.add_dir(name, tagpage, 'List', '')
+    utils.eod()
+
+
+@site.register()
+def Series(url):
+    listhtml = utils.getHtml(url)
+    match = re.compile(r'href="([^"]+)"\s+?title="([^"]+)".*?data-original="([^"]+)".*?videos">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for seriepage, name, img, videos in match:
+        name = utils.cleantext(name) + " [COLOR deeppink]" + videos.strip() + "[/COLOR]"
+        seriepage = seriepage + ajaxcommon
+        site.add_dir(name, seriepage, 'List', img, name)
+    np = re.compile(r':(\d+)">Next', re.DOTALL | re.IGNORECASE).search(listhtml)
+    if np:
+        np = np.group(1)
+        pagelookup = re.search(r"(from(?:_videos)?)=(\d+)", url)
+        if pagelookup:
+            page = pagelookup.group(2)
+            fromtxt = pagelookup.group(1)
+            url = url.replace(f"{fromtxt}={page}", f"{fromtxt}={np}")
+            site.add_dir(f'Next Page ({np})', url, 'Series', site.img_next)
+    utils.eod()
+
+
+@site.register()
+def Search(url, keyword=None):
+    if not keyword:
+        site.search_dir(url, 'Search')
+    else:
+        title = keyword.replace(' ', '-')
+        searchq = keyword.replace(' ', '+')
+        url = f"{url}{title}/?mode=async&function=get_block&block_id=list_videos_videos_list_search_result&q={searchq}&cat_ids=&sort_by=&from_videos=1"
+        List(url)
+
+
+@site.register()
+def Playvid(url, name, download=None):
+    vp = utils.VideoPlayer(name, download)
+    vp.progress.update(25, "[CR]Loading video page[CR]")
+
+    vpage = utils.getHtml(url, site.url)
+
+    videourl = None
+    sources = {}
+    license = re.compile(r"license_code:\s*'([^']+)", re.DOTALL | re.IGNORECASE).findall(vpage)[0]
+    patterns = [r"video_url:\s*'([^']+)[^;]+?video_url_text:\s*'([^']+)",
+                r"video_alt_url\d*?:\s*'([^']+)[^;]+?video_alt_url\d*?_text:\s*'([^']+)",
+                r"video_url:\s*'([^']+)',\s*postfix:\s*'\.mp4',\s*(preview)"]
+    for pattern in patterns:
+        items = re.compile(pattern, re.DOTALL | re.IGNORECASE).findall(vpage)
+        for surl, qual in items:
+            qual = '00' if qual == 'preview' else qual
+            surl = kvs_decode(surl, license)
+            sources.update({qual: surl})
+
+    if len(sources) > 0:
+        videourl = utils.selector('Select quality', sources, setting_valid='qualityask', sort_by=lambda x: 1081 if x == '4k' else int(x[:-1]), reverse=True)
+        if not videourl:
+            vp.progress.close()
+            return
+    else:
+        match = re.search(r'Download:\s*?<a href="([^"]+)"', vpage, re.IGNORECASE | re.DOTALL)
+        if match:
+            videourl = match.group(1)
+
+    if videourl:
+        vp.play_from_direct_link(videourl)
+    else:
+        vp.progress.close()
+        utils.notify('Oh Oh', 'No Videos found')
+
+
+@site.register()
+def Lookupinfo(url):
+    try:
+        listhtml = utils.getHtml(url)
+    except:
+        return None
+
+    infodict = {}
+
+    categories = re.compile('/(categories/[^"]+)">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    if categories:
+        for url, cat in categories:
+            cat = "Cat - " + cat.strip()
+            infodict[cat] = site.url + url
+    
+
+    tags = re.compile('/(tags[^"]+)">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    if tags:
+        for url, tag in tags:
+            tag = "Tag - " + tag.strip()
+            infodict[tag] = site.url + url
+            
+    series = re.compile('/(series/[^"]+)">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    if series:
+        for url, serie in series:
+            serie = "Serie - " + serie.strip()
+            infodict[serie] = site.url + url
+
+    if infodict:
+        selected_item = utils.selector('Choose item', infodict, show_on_one=True)
+        if not selected_item:
+            return
+        selected_item = selected_item + ajaxcommon
+        contexturl = (utils.addon_sys
+                      + "?mode=" + str('hentai-moon.List')
+                      + "&url=" + urllib_parse.quote_plus(selected_item))
+        utils.kodilog(f"selected_item: {selected_item}")
+        xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+    else:
+        utils.notify('Notify', 'No categories, tags or serie found for this video')
+    return
--- plugin.video.cumination/resources/lib/sites/seaporn.py
+++ plugin.video.cumination/resources/lib/sites/seaporn.py
@@ -22,39 +22,39 @@ from six.moves import urllib_parse
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('naughtyblog', '[COLOR hotpink]NaughtyBlog[/COLOR] [COLOR red][Debrid only]', 'https://www.naughtyblog.org/', 'https://www.naughtyblog.org/wp-content/images/logo/main_logo.png', 'naughtyblog')
+site = AdultSite('seaporn', '[COLOR hotpink]SeaPorn[/COLOR] [COLOR red][Debrid only][/COLOR]', 'https://www.seaporn.org/', '', 'seaporn')
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url + 'categories/', 'Categories', site.img_cat)
-    site.add_dir('[COLOR hotpink]Movies[/COLOR]', site.url + 'category/movies/', 'List', site.img_cat)
+    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url, 'Categories', site.img_cat)
     site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + '?s=', 'Search', site.img_search)
-    List(site.url + 'category/clips/')
+    List(site.url)
     utils.eod()
 
 
 @site.register()
 def List(url):
     listhtml = utils.getHtml(url, '')
-    match = re.compile('class="post-author">(.*?)post-content.*?<a href="([^"]+)"><img.*?src="([^"]+)".*?title="([^"]+)".*?<strong>([^<]+)<.*?<em>([^<]+)<.*?<p>([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    match = re.compile('entry-title"><a href="([^"]+)"[^>]+>([^<]+).*?<time[^>]+>([^<]+)<.*?src="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(listhtml)
     if not match:
         return
-    for tags, videopage, img, name, title, release, plot in match:
+    for videopage, name, posted, img in match:
         name = utils.cleantext(name)
-        plot = "{}\n{}\n{}".format(utils.cleantext(title), utils.cleantext(release), utils.cleantext(plot))
-        if any(tag in tags for tag in ['siterip', 'onlyfans-leak']):
+        plot = "{}\n{}".format(utils.cleantext(name), utils.cleantext(posted))
+
+        if 'static.keep2share' in img:
             continue
 
         contextmenu = []
         contexturl = (utils.addon_sys
-                          + "?mode=" + str('naughtyblog.Lookupinfo')
+                          + "?mode=" + str('seaporn.Lookupinfo')
                           + "&url=" + urllib_parse.quote_plus(videopage))
         contextmenu.append(('[COLOR deeppink]Lookup info[/COLOR]', 'RunPlugin(' + contexturl + ')'))
 
         site.add_download_link(name, videopage, 'Playvid', img, plot, contextm=contextmenu)
 
-    np = re.compile(r'aria-label="Next Page"\s*href="([^"]+)"', re.DOTALL | re.IGNORECASE).search(listhtml)
+    np = re.compile('page-numbers" href="([^"]+)">Next', re.DOTALL | re.IGNORECASE).search(listhtml)
     if np:
         page_number = np.group(1).split('/')[-2]
         site.add_dir('Next Page (' + page_number + ')', np.group(1), 'List', site.img_next)
@@ -66,14 +66,13 @@ def Playvid(url, name, download=None):
     vp = utils.VideoPlayer(name, download)
     vp.progress.update(25, "[CR]Loading video page[CR]")
     sitehtml = utils.getHtml(url, site.url)
-    downloads = re.compile('id="download">(.*?)</div>', re.DOTALL | re.IGNORECASE).findall(sitehtml)[0]
-    sources = re.compile(r'href="([^"]+)"\s+?title="([^\s]+)\s', re.DOTALL | re.IGNORECASE).findall(downloads)
+    sources = re.compile('<a href="([^"]+)" class="autohyperlink">https*://([^/]+)', re.DOTALL | re.IGNORECASE).findall(sitehtml)
     links = {}
     for link, hoster in sources:
+        utils.kodilog(f"link: {link} hoster: {hoster}")
         if vp.resolveurl.HostedMediaFile(link).valid_url():
-            linkparts = link.split('.')
-            quality = linkparts[-3] if link.endswith('.html') else linkparts[-2]
-            hoster = f"{hoster} {quality}"
+            filename = link.split('/')[-1]
+            hoster = f"{hoster} {filename}"
             links[hoster] = link
     videourl = utils.selector('Select link', links)
     if not videourl:
@@ -96,9 +95,12 @@ def Search(url, keyword=None):
 @site.register()
 def Categories(url):
     listhtml = utils.getHtml(url)
-    match = re.compile('href="([^"]+)"[^>]+>([^<]+)<span class="pocetvideicat">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    match = re.compile(r'cat-item-\d+"><a\s+?href="([^"]+)">([^<]+)</a>\s+?\(([^\)]+)\)', re.DOTALL | re.IGNORECASE).findall(listhtml)
     for catpage, name, videos in match:
         name = utils.cleantext(name.strip())
+        if any(cat in name for cat in ['Galleries', 'Magazines', 'Pictures', 'Siterips', 'Mobile']):
+            continue
+        
         name = f"{name} - {videos.strip()} videos"
         site.add_dir(name, catpage, 'List', '')
     utils.eod()
@@ -113,32 +115,26 @@ def Lookupinfo(url):
 
     infodict = {}
 
-    categories = re.compile(r'/((?:category|tag)/[^"]+)"\s*?rel="(?:category )*?tag">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    categories = re.compile(r'/(category/[^"]+)"\s*?rel="category tag">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
     if categories:
         for url, cat in categories:
             cat = "Cat - " + cat.strip()
             infodict[cat] = site.url + url
 
-    sites = re.compile(r'(site/[^"]+)"\s*?rel="tag">([^<]+)', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    if sites:
-        for url, sitename in sites:
-            sitename = "Site - " + sitename.strip()
-            infodict[sitename] = site.url + url
-
-    models = re.compile(r'/(pornstar/[^"]+)"\s*?rel="tag">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    if models:
-        for url, model in models:
-            model = "Model - " + model.strip()
-            infodict[model] = site.url + url
+    tags = re.compile(r'/(tag/[^"]+)"\s*?rel="tag">([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    if tags:
+        for url, tag in tags:
+            tag = "Tag - " + tag.strip()
+            infodict[tag] = site.url + url
 
     if infodict:
         selected_item = utils.selector('Choose item', infodict, show_on_one=True)
         if not selected_item:
             return
         contexturl = (utils.addon_sys
-                      + "?mode=" + str('naughtyblog.List')
+                      + "?mode=" + str('seaporn.List')
                       + "&url=" + urllib_parse.quote_plus(selected_item))
         xbmc.executebuiltin('Container.Update(' + contexturl + ')')
     else:
-        utils.notify('Notify', 'No models, categories or sites found for this video')
+        utils.notify('Notify', 'No tags or categories found for this video')
     return
