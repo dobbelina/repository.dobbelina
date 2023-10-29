d063ca8ce7d5368f4fa3e9f62b257356caf4f4e8
dobbelina <46063764+dobbelina@users.noreply.github.com>
2023-10-29 04:16:57 +0000

    fix hpjav

--- plugin.video.cumination/resources/lib/sites/hpjav.py
+++ plugin.video.cumination/resources/lib/sites/hpjav.py
@@ -18,24 +18,23 @@
 
 import re
 import base64
-import json
 from six.moves import urllib_parse
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('hpjav', '[COLOR hotpink]HPJav[/COLOR]', 'https://hpjav.tv/', 'https://hpjav.tv/images/HP.png', 'hpjav')
+site = AdultSite('hpjav', '[COLOR hotpink]HPJav[/COLOR]', 'https://hpjav.in/', 'https://hpjav.in/wp-content/themes/HPJAV/images/logo.png', 'hpjav')
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url + 'allcategories/', 'Cat', site.img_cat)
-    site.add_dir('[COLOR hotpink]Censored[/COLOR]', site.url + 'category/censored', 'List', site.img_search)
-    site.add_dir('[COLOR hotpink]Unensored[/COLOR]', site.url + 'category/uncensored', 'List', site.img_search)
-    site.add_dir('[COLOR hotpink]Amateur[/COLOR]', site.url + 'category/amateur-jav', 'List', site.img_search)
-    site.add_dir('[COLOR hotpink]Chinese Subtitles[/COLOR]', site.url + 'category/chinese-subtitles', 'List', site.img_search)
+    site.add_dir('[COLOR hotpink]Censored[/COLOR]', site.url + 'censored/', 'List', site.img_search)
+    site.add_dir('[COLOR hotpink]Unensored[/COLOR]', site.url + 'uncensored/', 'List', site.img_search)
+    site.add_dir('[COLOR hotpink]Amateur[/COLOR]', site.url + 'amature/', 'List', site.img_search)
+    site.add_dir('[COLOR hotpink]FC2 PPV[/COLOR]', site.url + 'fc2ppv/', 'List', site.img_search)
+    site.add_dir('[COLOR hotpink]VR[/COLOR]', site.url + 'vr/', 'List', site.img_search)
     site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + '?s=', 'Search', site.img_search)
 
-    List(site.url + 'hot/')
+    List(site.url + 'trend/')
 
 
 @site.register()
@@ -44,24 +43,19 @@ def List(url):
         listhtml = utils.getHtml(url)
     except:
         return None
-    match = re.compile(r'class="video-item".+?href="([^"]+).+?original="([^"]+).+?href.+?>([^<]+)', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for videopage, img, name in match:
-        img = img + '|Referer=' + url
+    match = re.compile(r'<a\shref="([^"]+)"><div\s*class="post-list-image"><img\s*src="([^"]+).+?duration">([^<.]+).+?span>([^<]+)', re.DOTALL | re.IGNORECASE).findall(listhtml)
+    for videopage, img, duration, name in match:
         name = utils.cleantext(name)
-        if videopage.startswith('/'):
-            videopage = urllib_parse.urljoin(site.url, videopage)
-        site.add_download_link(name, videopage, 'Playvid', img, name)
-    try:
-        next_page = re.compile(r'''href=['"]?([^'">]+)['"]?><span[^>]+>&raquo;''').findall(listhtml)[0]
-        last_page = re.compile(r"href='([^']+).+?>\s*Last").findall(listhtml)
-        if last_page:
-            last = re.findall(r'\d+', last_page[0])[-1]
-        page_nr = re.findall(r'\d+', next_page)[-1]
-        if next_page.startswith('/'):
-            next_page = urllib_parse.urljoin(site.url, next_page)
-        site.add_dir('Next Page (' + page_nr + ('/' + last if last_page else '') + ')', next_page, 'List', site.img_next)
-    except:
-        pass
+        site.add_download_link(name, videopage, 'Playvid', img, name, duration=duration)
+
+    pagination = re.search(r"(<div\s*class='wp-pagenavi'.+?</div>)", listhtml, re.DOTALL)
+    if pagination:
+        pagination = pagination.group(1)
+        if "Next Page" in pagination:
+            pgurl = re.findall(r'"Next\s*Page"\s*href="([^"]+)', pagination)[0]
+            pgtxt = re.findall(r"'pages'>([^<]+)", pagination)[0]
+            site.add_dir('Next Page.. (Currently in Page {0})'.format(pgtxt), pgurl, 'List', site.img_next)
+
     utils.eod()
 
 
@@ -93,12 +87,11 @@ def Playvid(url, name, download=None):
     vp = utils.VideoPlayer(name, download)
     vp.progress.update(25, "[CR]Loading video page[CR]")
     videopage = utils.getHtml(url, site.url)
+    videopage += utils.get_packed_data(videopage)
 
-    eurls = re.compile(r'var\s*vser\s*=[^"]+"([^"]+)', re.DOTALL | re.IGNORECASE).findall(videopage)[0]
-    eurls = json.loads(base64.b64decode(eurls).decode())
+    eurls = re.compile(r'id="embed"\s*src="([^"]+)').findall(videopage)
     sources = {}
     for eurl in eurls:
-        eurl = hpjav_decode(eurl)
         if vp.resolveurl.HostedMediaFile(eurl):
             sources.update({eurl.split('/')[2]: eurl})
     videourl = utils.selector('Select Hoster', sources)
