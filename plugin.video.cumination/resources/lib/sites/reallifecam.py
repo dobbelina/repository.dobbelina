40f13b885523335bb4492e444e47ec2ebd17e18b
dobbelina <46063764+dobbelina@users.noreply.github.com>
2024-06-21 14:32:38 +0000

    voyeur-house.life, voyeurhouse.life - new domain

--- plugin.video.cumination/resources/lib/sites/reallifecam.py
+++ plugin.video.cumination/resources/lib/sites/reallifecam.py
@@ -24,8 +24,8 @@ from resources.lib.adultsite import AdultSite
 
 site = AdultSite('rlc', '[COLOR hotpink]Reallifecam.to[/COLOR]', 'https://reallifecam.to/', 'https://reallifecam.to/images/logo/logo.png', 'rlc')
 site1 = AdultSite('vh', '[COLOR hotpink]Voyeur-house.to[/COLOR]', 'https://voyeur-house.to/', 'https://voyeur-house.to/images/logo/logo.png', 'vh')
-site2 = AdultSite('vhlife', '[COLOR hotpink]Voyeur-house.life[/COLOR]', 'https://www.voyeur-house.life/', 'https://www.voyeur-house.life/images/logo/logo.png', 'vhlife')
-site3 = AdultSite('vhlife1', '[COLOR hotpink]Voyeurhouse.life[/COLOR]', 'https://www.voyeurhouse.life/', 'https://www.voyeurhouse.life/images/logo/logo.png', 'vhlife1')
+site2 = AdultSite('vhlife', '[COLOR hotpink]Voyeur-house.cc[/COLOR]', 'https://www.voyeur-house.cc/', 'https://www.voyeur-house.cc/images/logo/logo.png', 'vhlife')
+site3 = AdultSite('vhlife1', '[COLOR hotpink]Reallifecams.in[/COLOR]', 'https://www.reallifecams.in/', 'https://www.reallifecams.in/images/logo/logo.png', 'vhlife1')
 site4 = AdultSite('camcaps', '[COLOR hotpink]Camcaps.to[/COLOR]', 'https://camcaps.to/', 'https://camcaps.to/images/logo/logo.png', 'camcapsto')
 
 
@@ -34,9 +34,9 @@ def getBaselink(url):
         siteurl = site.url
     elif 'voyeur-house.to' in url:
         siteurl = site1.url
-    elif 'voyeur-house.life' in url:
+    elif 'voyeur-house.cc' in url:
         siteurl = site2.url
-    elif 'voyeurhouse.life' in url:
+    elif 'reallifecams.in' in url:
         siteurl = site3.url
     elif 'camcaps.to' in url:
         siteurl = site4.url
