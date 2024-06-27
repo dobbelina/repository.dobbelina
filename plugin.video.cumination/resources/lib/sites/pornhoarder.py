03e29c6ed46c1dc331194d7f02cc4d84b1721658
dobbelina <46063764+dobbelina@users.noreply.github.com>
2024-06-27 14:52:13 +0000

    pornhoarder - search, categories - fixes  #1368

--- plugin.video.cumination/resources/lib/sites/pornhoarder.py
+++ plugin.video.cumination/resources/lib/sites/pornhoarder.py
@@ -20,10 +20,10 @@ import re
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('pornhoarder', '[COLOR hotpink]PornHoarder[/COLOR]', 'https://ww4.pornhoarder.tv/', 'pornhoarder.jpg', 'pornhoarder')
+site = AdultSite('pornhoarder', '[COLOR hotpink]PornHoarder[/COLOR]', 'https://ww5.pornhoarder.tv/', 'pornhoarder.jpg', 'pornhoarder')
 
 ph_headers = {
-    'Origin': 'https://ww4.pornhoarder.tv',
+    'Origin': 'https://ww5.pornhoarder.tv',
     'User-Agent': utils.USER_AGENT,
     'X-Requested-With': 'XMLHttpRequest',
 }
@@ -43,7 +43,7 @@ def Main():
 def List(url, page=1):
     search = '' if url.startswith('https://') else url
     data = Createdata(page, search)
-    listhtml = utils.postHtml('https://ww4.pornhoarder.tv/ajax_search.php', headers=ph_headers, form_data=data)
+    listhtml = utils.postHtml('https://ww5.pornhoarder.tv/ajax_search.php', headers=ph_headers, form_data=data)
     match = re.compile('href="([^"]+)".*?data-src="([^"]+)"(.*?)h1>([^<]+)<', re.DOTALL | re.IGNORECASE).findall(listhtml)
     if not match:
         return
@@ -102,6 +102,8 @@ def Createdata(page=1, search=''):
         ('servers[]', '25'),
         ('servers[]', '41'),
         ('servers[]', '17'),
+        ('servers[]', '42'),
+        ('servers[]', '43'),
         ('author', '0'),
         ('page', page),
     ]
