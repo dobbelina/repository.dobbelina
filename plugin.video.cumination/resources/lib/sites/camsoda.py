3091a47e512121727f0d35696b1f3a71155eadf2
dobbelina <46063764+dobbelina@users.noreply.github.com>
2024-07-27 21:45:13 +0000

    camsoda - add categories. Fixes #1377

--- plugin.video.cumination/resources/lib/sites/camsoda.py
+++ plugin.video.cumination/resources/lib/sites/camsoda.py
@@ -21,57 +21,60 @@ from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
 site = AdultSite('camsoda', '[COLOR hotpink]Camsoda[/COLOR]', 'https://www.camsoda.com', 'camsoda.png', 'camsoda', True)
+addon = utils.addon
 
 
 @site.register(default_mode=True)
 def Main():
+    female = addon.getSetting("chatfemale") == "true"
+    male = addon.getSetting("chatmale") == "true"
+    couple = addon.getSetting("chatcouple") == "true"
+    trans = addon.getSetting("chattrans") == "true"
     site.add_dir('[COLOR red]Refresh Camsoda images[/COLOR]', '', 'clean_database', '', Folder=False)
-    List(site.url + '/api/v1/browse/online')
+    if female:
+        site.add_dir('[COLOR violet]Female[/COLOR]', site.url + '/api/v1/browse/react?gender-hide=c,t,m', 'List', '', 1)
+        site.add_dir('[COLOR hotpink]North America - Female[/COLOR]', site.url + '/api/v1/browse/react/girls/region/usa?gender-hide=c,t,m', 'List', '', 1)
+        site.add_dir('[COLOR hotpink]Central/South America - Female[/COLOR]', site.url + '/api/v1/browse/react/girls/region/central-south-america?gender-hide=c,t,m', 'List', '', 1)
+        site.add_dir('[COLOR hotpink]Europe - Female[/COLOR]', site.url + '/api/v1/browse/react/girls/region/europe?gender-hide=c,t,m', 'List', '', 1)
+        site.add_dir('[COLOR hotpink]Eastern Europe - Female[/COLOR]', site.url + '/api/v1/browse/react/girls/region/eastern-europe?gender-hide=c,t,m', 'List', '', 1)
+        site.add_dir('[COLOR hotpink]Asia - Female[/COLOR]', site.url + '/api/v1/browse/react/girls/region/asia?gender-hide=c,t,m', 'List', '', 1)
+    if couple:
+        site.add_dir('[COLOR violet]Couple[/COLOR]', site.url + '/api/v1/browse/react?gender-hide=f,t,m', 'List', '', 1)
+    if male:
+        site.add_dir('[COLOR violet]Male[/COLOR]', site.url + '/api/v1/browse/react?gender-hide=c,t,f', 'List', '', 1)
+    if trans:
+        site.add_dir('[COLOR violet]Transexual[/COLOR]', site.url + '/api/v1/browse/react?gender-hide=c,f,m', 'List', '', 1)
+
     utils.eod()
 
 
 @site.register()
-def List(url):
+def List(url, page):
     if utils.addon.getSetting("chaturbate") == "true":
         clean_database(False)
-    response = utils._getHtml(url)
-    camgirls = json.loads(response)['results']
+    response = utils._getHtml(url + '&p={}'.format(page))
+    jd = json.loads(response)
+    camgirls = jd.get('userList')
     for camgirl in camgirls:
-        camgirl = camgirl.get('tpl')
-        if isinstance(camgirl, dict):
-            name = camgirl.get('2')
-            name = name if utils.PY3 else name.encode('utf8')
-            subject = camgirl.get('6')
-            subject += u'[CR][CR][COLOR deeppink] Viewers: [/COLOR]{}[CR]'.format(camgirl.get('4'))
-            if camgirl.get('3'):
-                subject += u'[COLOR deeppink] Status: [/COLOR]{}[CR]'.format(camgirl.get('3'))
-            subject = subject if utils.PY3 else subject.encode('utf8')
-            cid = camgirl.get('1')
-            img = camgirl.get('10')
-            # if img:
-            #     img = 'http:' + img
-            fanart = camgirl.get('15')
-            # if fanart:
-            #     fanart = 'http:' + fanart
-        else:
-            name = camgirl[2]
-            name = name if utils.PY3 else name.encode('utf8')
-            subject = camgirl[6]
-            subject += u'[CR][CR][COLOR deeppink] Viewers: [/COLOR]{}[CR]'.format(camgirl[6])
-            if camgirl[3]:
-                subject += u'[COLOR deeppink] Status: [/COLOR]{}[CR]'.format(camgirl[3])
-            subject = subject if utils.PY3 else subject.encode('utf8')
-            cid = camgirl[1]
-            # img = 'http:' + camgirl[10]
-            img = camgirl[10]
-            if len(camgirl) < 16:
-                fanart = None
-            else:
-                # fanart = 'http:' + camgirl[15]
-                fanart = camgirl[15]
-
-        videourl = '{0}/api/v1/video/vtoken/{1}'.format(site.url, cid)
+        name = camgirl.get('displayName')
+        name = name if utils.PY3 else name.encode('utf8')
+        subject = camgirl.get('subjectText')
+        subject += u'[CR][CR][COLOR deeppink] Viewers: [/COLOR]{}[CR]'.format(camgirl.get('connectionCount'))
+        if camgirl.get('status'):
+            subject += u'[COLOR deeppink] Status: [/COLOR]{}[CR]'.format(camgirl.get('status'))
+        subject = subject if utils.PY3 else subject.encode('utf8')
+        # cid = camgirl.get('id')
+        username = camgirl.get('username')
+        img = camgirl.get('thumbUrl')
+        fanart = camgirl.get('offlinePictureUrl')
+        # videourl = '{0}/api/v1/video/vtoken/{1}'.format(site.url, cid)
+        videourl = '{0}/api/v1/chat/react/{1}'.format(site.url, username)
         site.add_download_link(name, videourl, 'Playvid', img, subject, noDownload=True, fanart=fanart)
+    perPageCount = jd.get('perPageCount')
+    totalCount = jd.get('totalCount')
+    if (perPageCount * page) < totalCount:
+        lastpg = -1 * (-totalCount // perPageCount)
+        site.add_dir('Next Page.. (Currently in Page {0} of {1})'.format(page, lastpg), url, 'List', site.img_next, page + 1)
     utils.eod()
 
 
@@ -96,22 +99,14 @@ def clean_database(showdialog=True):
 
 @site.register()
 def Playvid(url, name):
-    url = url + "?username=guest_" + str(random.randrange(100, 55555))
-    response = utils._getHtml(url)
-    data = json.loads(response)
-    if "enc" in data['stream_name']:
-        if len(data['edge_servers']) > 0:
-            videourl = "https://" + random.choice(data['edge_servers']) + "/" + data['app'] + "/mp4:" + data['stream_name'] + "_h264_aac_480p/playlist.m3u8?token=" + data['token']
-        else:
-            videourl = ""
-            utils.notify('Finished', 'Model gone Offline')
-    else:
-        if len(data['edge_servers']) > 0:
+    if 'm3u8' not in url:
+        url = url + "?username=guest_" + str(random.randrange(100, 55555))
+        response = json.loads(utils._getHtml(url))
+        data = response.get('stream')
+        if len(data.get('edge_servers', [])) > 0:
             videourl = "https://" + random.choice(data['edge_servers']) + "/" + data['stream_name'] + "_v1/index.m3u8?token=" + data['token']
+            videourl += '|User-Agent=iPad&verifypeer=false'
+            vp = utils.VideoPlayer(name)
+            vp.play_from_direct_link(videourl)
         else:
-            videourl = ""
             utils.notify('Finished', 'Model gone Offline or Private')
-    if videourl:
-        videourl += '|User-Agent=iPad&verifypeer=false'
-        vp = utils.VideoPlayer(name)
-        vp.play_from_direct_link(videourl)
