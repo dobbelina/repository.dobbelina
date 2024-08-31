f46eb99a195dedd1235f31f451d00a84c26b4240
dobbelina <46063764+dobbelina@users.noreply.github.com>
2024-08-31 12:51:54 +0000

    ividz - fix listing #1409

--- plugin.video.cumination/resources/lib/sites/ividz.py
+++ plugin.video.cumination/resources/lib/sites/ividz.py
@@ -17,18 +17,21 @@
 '''
 
 import re
+import xbmc
+import xbmcgui
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
+from six.moves import urllib_parse
 
-site = AdultSite('ividz', '[COLOR hotpink]Ividz[/COLOR]', 'https://www.incestvidz.com/', 'https://www.incestvidz.com/wp-content/uploads/logo-1.jpg', 'ividz')
+site = AdultSite('ividz', '[COLOR hotpink]Ividz[/COLOR]', 'https://www.incestvidz.com/', 'ividz.png', 'ividz')
 
 addon = utils.addon
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url, 'Cat', '', '')
-    site.add_dir('[COLOR hotpink]Tags[/COLOR]', site.url + 'tags/', 'Tag', '', '')
+    site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url + 'categories/', 'Cat', '', '')
+    # site.add_dir('[COLOR hotpink]Tags[/COLOR]', site.url + 'tags/', 'Tag', '', '')
     site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + '?s=', 'Search', site.img_search)
     List(site.url + '?orderby=date')
 
@@ -36,25 +39,54 @@ def Main():
 @site.register()
 def List(url):
     listhtml = utils.getHtml(url)
-    match = re.compile(r'class="clip-link" data-id="\d+" title="([^"]+)" href="([^"]+)".*?src="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for name, videopage, img in match:
-        name = utils.cleantext(name)
 
-        site.add_download_link(name, videopage, 'Play', img, name)
+    if 'Sorry, but nothing matched your search terms' in listhtml:
+        utils.notify(msg='Nothing found')
+        utils.eod()
+        return
 
-    np = re.compile('Next page" href="([^"]+)"', re.DOTALL | re.IGNORECASE).search(listhtml)
-    if np:
-        site.add_dir('Next Page... ({0})'.format(np.group(1).split('/')[-2]), np.group(1), 'List', site.img_next)
+    delimiter = '<article id="post'
+    re_videopage = 'href="([^"]+)"'
+    re_name = 'title="([^"]+)"'
+    re_img = 'src="([^"]+)"'
+    re_duration = 'class="duration">([^<]+)<'
+    skip = 'I like this'
+    utils.videos_list(site, 'ividz.Play', listhtml, delimiter, re_videopage, re_name, re_img, re_duration=re_duration, contextm='ividz.Related', skip=skip)
+
+    re_npurl = r'class="current">\d+</a></li><li><a href="([^"]+)"'
+    re_npnr = r'class="current">\d+</a></li><li><a href="[^"]+" class="inactive">(\d+)<'
+    re_lpnr = r'page/(\d+)/[^>]*>Last<'
+    utils.next_page(site, 'ividz.List', listhtml, re_npurl, re_npnr, re_lpnr=re_lpnr, contextm='ividz.GotoPage')
     utils.eod()
 
 
 @site.register()
+def Related(url):
+    contexturl = (utils.addon_sys + "?mode=" + str('ividz.List') + "&url=" + urllib_parse.quote_plus(url))
+    xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+
+
+@site.register()
+def GotoPage(list_mode, url, np, lp):
+    dialog = xbmcgui.Dialog()
+    pg = dialog.numeric(0, 'Enter Page number')
+    if pg:
+        url = url.replace('/page/{}/'.format(np), '/page/{}/'.format(pg))
+        if int(lp) > 0 and int(pg) > int(lp):
+            utils.notify(msg='Out of range!')
+            return
+        contexturl = (utils.addon_sys + "?mode=" + str(list_mode) + "&url=" + urllib_parse.quote_plus(url))
+        xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+
+
+@site.register()
 def Cat(url):
+
     cathtml = utils.getHtml(url, site.url)
-    match = re.compile(r'/(category[^ ]+)">([^<]+)', re.DOTALL | re.IGNORECASE).findall(cathtml)
-    for catpage, name in match:
+    match = re.compile(r'article id="post.+?href="([^"]+)" title="([^"]+)".+?src="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(cathtml)
+    for catpage, name, img in match:
         name = utils.cleantext(name)
-        site.add_dir(name, site.url + catpage + '?orderby=date', 'List')
+        site.add_dir(name, catpage + '?orderby=date', 'List', img)
     utils.eod()
 
 
