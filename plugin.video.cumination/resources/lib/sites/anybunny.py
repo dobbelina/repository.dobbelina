6e16879ca40f500803a8b67383efdfb161a355c4
dobbelina <46063764+dobbelina@users.noreply.github.com>
2026-03-07 13:49:58 +0000

    anybunny - fixes #1773

--- plugin.video.cumination/resources/lib/sites/anybunny.py
+++ plugin.video.cumination/resources/lib/sites/anybunny.py
@@ -17,47 +17,84 @@
 '''
 
 import re
+
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
+from six.moves import urllib_parse
+import xbmc
+import xbmcgui
+
 
 site = AdultSite("anybunny", "[COLOR hotpink]Anybunny[/COLOR]", "http://anybunny.org/", "anybunny.png", "anybunny")
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Top videos[/COLOR]', site.url + 'top/', 'List', '', '')
-    site.add_dir('[COLOR hotpink]Categories - images[/COLOR]', site.url, 'Categories', site.img_cat)
+    # site.add_dir('[COLOR hotpink]Categories - images[/COLOR]', site.url, 'Categories', site.img_cat)
     site.add_dir('[COLOR hotpink]Categories - all[/COLOR]', site.url, 'Categories2', site.img_cat)
     site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + 'new/', 'Search', site.img_search)
-    List(site.url + 'new/?p=1')
+    # List(site.url + 'new/twins')
+    Categories(site.url)
     utils.eod()
 
 
 @site.register()
 def List(url):
-    listhtml = utils.getHtml(url, '')
-    match = re.compile(r"class='nuyrfe'\s*href='([^']+).+?src='([^']+).+?alt='([^']+)", re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for videopage, img, name in match:
-        name = utils.cleantext(name)
-        site.add_download_link(name, videopage, 'Playvid', img, '')
+    try:
+        listhtml = utils.getHtml(url, '')
+    except:
+        utils.notify(msg='No videos found!')
+        return
 
-    nextp = re.compile('href="([^"]+)">Next', re.DOTALL | re.IGNORECASE).search(listhtml)
-    if nextp:
-        site.add_dir('Next Page', nextp.group(1), 'List', site.img_next)
+    delimiter = r"<li\s+data-id='|class='nuyrfe"
+    re_videopage = "href='([^']+)'"
+    re_name = "alt='([^']+)'"
+    re_img = r"src='([^']+)'"
+    re_duration = r"'>([\d:]+)</div>"
+    re_quality = r"'>(HD)\s*</div>"
 
+    cm = []
+    cm_related = (utils.addon_sys + "?mode=anybunny.Related&url=")
+    cm.append(('[COLOR deeppink]Related videos[/COLOR]', 'RunPlugin(' + cm_related + ')'))
+    utils.videos_list(site, 'anybunny.Playvid', listhtml, delimiter, re_videopage, re_name, re_img, re_duration=re_duration, re_quality=re_quality, contextm=cm)
+
+    re_npurl = 'href="([^"]+)">Next'
+    re_npnr = r'\?p=(\d+)">Next'
+    utils.next_page(site, 'anybunny.List', listhtml, re_npurl, re_npnr, contextm='anybunny.GotoPage')
     utils.eod()
 
 
 @site.register()
 def Playvid(url, name, download=None):
-    vp = utils.VideoPlayer(name, download, direct_regex=r"source src=\\'([^']+)\\'")
-    vp.play_from_site_link(url)
+    vp = utils.VideoPlayer(name, download, direct_regex=r"<iframe.+?src='([^']+)'")
+    vpage = utils.getHtml(url, site.url)
+    if not re.match(r'id:\s*"player",\s*file:', vpage, re.DOTALL | re.IGNORECASE):
+        match = re.search(r"<iframe.+?src='([^']+)'", vpage, re.DOTALL | re.IGNORECASE)
+        if match:
+            iframe_url = match.group(1)
+            vpage = utils.getHtml(iframe_url, site.url)
+
+    file = re.search(r'file:\s*"([^"]+")', vpage, re.DOTALL | re.IGNORECASE)
+    if not file:
+        return
+    match = re.compile(r'\[(\d+)\](.+?)[,"]', re.DOTALL | re.IGNORECASE).findall(file.group(1))
+    if match:
+        sources = {quality: video_url for quality, video_url in match}
+        video_url = utils.prefquality(sources, sort_by=lambda x: int(x), reverse=True)
+    else:
+        video_url = re.search(r'file:\s*["]([^"]+)', vpage, re.DOTALL | re.IGNORECASE)
+        if video_url:
+            video_url = video_url.group(1)
+
+    if video_url:
+        video_url = video_url.split(' or ')[-1].split(":cast:")[0]
+        vp.play_from_direct_link(video_url + '|referer={0}'.format(url))
 
 
 @site.register()
 def Categories(url):
     cathtml = utils.getHtml(url, '')
-    match = re.compile("<a href='/top/([^']+)'>.*?src='([^']+)' alt='([^']+)'", re.DOTALL | re.IGNORECASE).findall(cathtml)
+    match = re.compile("href='/top/([^']+)'>.*?src='([^']+)' alt='([^']+)'", re.DOTALL | re.IGNORECASE).findall(cathtml)
     match = sorted(match, key=lambda x: x[2])
     for catid, img, name in match:
         catpage = site.url + 'new/' + catid
@@ -78,10 +115,26 @@ def Categories2(url):
 
 @site.register()
 def Search(url, keyword=None):
-    searchUrl = url
     if not keyword:
         site.search_dir(url, 'Search')
     else:
-        title = keyword.replace(' ', '_')
-        searchUrl = searchUrl + title
-        List(searchUrl)
+        List(url + keyword.replace(' ', '_'))
+
+
+@site.register()
+def GotoPage(url, np, lp=0):
+    dialog = xbmcgui.Dialog()
+    pg = dialog.numeric(0, 'Enter Page number')
+    if pg:
+        if int(lp) > 0 and int(pg) > int(lp):
+            utils.notify(msg='Out of range!')
+            return
+        url = re.sub(r'\?p=\d+', r'?p={}'.format(pg), url, re.IGNORECASE)
+        contexturl = (utils.addon_sys + "?mode=" + "anybunny.List&url=" + urllib_parse.quote_plus(url))
+        xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+
+
+@site.register()
+def Related(url):
+    contexturl = (utils.addon_sys + "?mode=" + str('anybunny.List') + "&url=" + urllib_parse.quote_plus(url))
+    xbmc.executebuiltin('Container.Update(' + contexturl + ')')
