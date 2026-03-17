916c79801c397d7e1f2a3440cd4163ecb8a7cd61
Ryan <rpeters1428@gmail.com>
2026-03-16 18:40:02 -0700

    Fix PMVHaven listing parsing

--- plugin.video.cumination/resources/lib/sites/pmvhaven.py
+++ plugin.video.cumination/resources/lib/sites/pmvhaven.py
@@ -1,19 +1,19 @@
 """
-    Cumination site scraper
-    Copyright (C) 2026 Team Cumination
+Cumination site scraper
+Copyright (C) 2026 Team Cumination
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import json
@@ -21,17 +21,53 @@ import re
 from six.moves import urllib_parse
 import xbmc
 import xbmcgui
+
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
 
-site = AdultSite('pmvhaven', '[COLOR hotpink]PMVHaven[/COLOR]', 'https://pmvhaven.com/', 'https://pmvhaven.com/pmvhavenlogo.png', 'pmvhaven')
+site = AdultSite(
+    "pmvhaven",
+    "[COLOR hotpink]PMVHaven[/COLOR]",
+    "https://pmvhaven.com/",
+    "pmvhaven.png",
+    "pmvhaven",
+)
+
+
+def _format_duration(video):
+    duration = video.get("duration")
+    if duration:
+        return duration
+
+    duration_seconds = video.get("durationSeconds")
+    if duration_seconds in (None, ""):
+        return ""
+
+    try:
+        duration_seconds = int(duration_seconds)
+    except (TypeError, ValueError):
+        return ""
+
+    hours, remainder = divmod(duration_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    if hours:
+        return "{}:{:02d}:{:02d}".format(hours, minutes, seconds)
+    return "{}:{:02d}".format(minutes, seconds)
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + 'api/videos/search?limit=32&page=1&q=', 'Search', site.img_search)
-    List(site.url + 'api/videos?limit=32&sort=-releaseDate&page=1&skipCount=true&tagMode=OR&expandTags=false')
+    site.add_dir(
+        "[COLOR hotpink]Search[/COLOR]",
+        site.url + "api/videos/search?limit=32&page=1&q=",
+        "Search",
+        site.img_search,
+    )
+    List(
+        site.url
+        + "api/videos?limit=32&sort=-releaseDate&page=1&skipCount=true&tagMode=OR&expandTags=false"
+    )
     utils.eod()
 
 
@@ -39,32 +75,67 @@ def Main():
 def List(url):
     listhtml = utils.getHtml(url, site.url)
     jdata = json.loads(listhtml)
+    if not isinstance(jdata, dict):
+        jdata = {}
 
-    for video in jdata.get('videos', []):
-        name = video['title']
-        img = video['thumbnailUrl']
-        video_url = video.get('videoUrl', video.get('hlsMasterPlaylistUrl', ''))
-        duration = video['duration']
-        quality = str(video['height']) + 'p'
-        extreme = video.get('hasExtremeContent', False)
-        if extreme:
-            name += '[COLOR red] EXTREME[/COLOR]'
-        tags = ','.join(video.get('tags', []))
-        models = ','.join(video.get('starsTags', []))
+    for video in jdata.get("videos", []):
+        name = video.get("title", "")
+        img = video.get("thumbnailUrl", "")
+        video_url = video.get("videoUrl", video.get("hlsMasterPlaylistUrl", ""))
+        duration = _format_duration(video)
+        height = video.get("height")
+        quality = "{}p".format(height) if height else ""
+        if not name or not video_url:
+            continue
+        if video.get("hasExtremeContent", False):
+            name += "[COLOR red] EXTREME[/COLOR]"
+        tags = ",".join(video.get("tags", []))
+        models = ",".join(video.get("starsTags", []))
 
-        cm = []
-        cm_lookupinfo = (utils.addon_sys + "?mode=pmvhaven.Lookupinfo&url={}".format(urllib_parse.quote_plus(tags + '|' + models)))
-        cm.append(('[COLOR deeppink]Lookup info[/COLOR]', 'RunPlugin({})'.format(cm_lookupinfo)))
+        cm_lookupinfo = (
+            utils.addon_sys
+            + "?mode=pmvhaven.Lookupinfo&url={}".format(
+                urllib_parse.quote_plus(tags + "|" + models)
+            )
+        )
+        cm = [
+            (
+                "[COLOR deeppink]Lookup info[/COLOR]",
+                "RunPlugin({})".format(cm_lookupinfo),
+            )
+        ]
 
-        site.add_download_link(name, video_url, 'Playvid', img, name, duration=duration, quality=quality, contextm=cm)
+        site.add_download_link(
+            name,
+            video_url,
+            "Playvid",
+            img,
+            name,
+            duration=duration,
+            quality=quality,
+            contextm=cm,
+        )
 
-    if jdata['pagination']['hasNext']:
-        next_page = jdata['pagination']['page'] + 1
-        next_url = re.sub(r'page=\d+', 'page={}'.format(next_page), url)
-        cm_page = (utils.addon_sys + "?mode=pmvhaven.GotoPage" + "&url=" + urllib_parse.quote_plus(next_url))
-        cm = [('[COLOR violet]Goto Page #[/COLOR]', 'RunPlugin(' + cm_page + ')')]
+    pagination = jdata.get("pagination")
+    if not isinstance(pagination, dict):
+        pagination = {}
 
-        site.add_dir('Next Page ({})'.format(next_page), next_url, 'List', site.img_next, contextm=cm)
+    if pagination.get("hasNext"):
+        next_page = pagination.get("page", 1) + 1
+        next_url = re.sub(r"page=\d+", "page={}".format(next_page), url)
+        cm_page = (
+            utils.addon_sys
+            + "?mode=pmvhaven.GotoPage&url="
+            + urllib_parse.quote_plus(next_url)
+        )
+        cm = [("[COLOR violet]Goto Page #[/COLOR]", "RunPlugin(" + cm_page + ")")]
+        site.add_dir(
+            "Next Page ({})".format(next_page),
+            next_url,
+            "List",
+            site.img_next,
+            contextm=cm,
+        )
 
     utils.eod()
 
@@ -72,11 +143,11 @@ def List(url):
 @site.register()
 def GotoPage(url):
     dialog = xbmcgui.Dialog()
-    pg = dialog.numeric(0, 'Enter Page number')
+    pg = dialog.numeric(0, "Enter Page number")
     if pg:
-        url = re.sub(r'page=\d+', 'page={}'.format(pg), url)
-        contexturl = (utils.addon_sys + "?mode=" + "pmvhaven.List&url=" + urllib_parse.quote_plus(url))
-        xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+        url = re.sub(r"page=\d+", "page={}".format(pg), url)
+        contexturl = utils.addon_sys + "?mode=pmvhaven.List&url=" + urllib_parse.quote_plus(url)
+        xbmc.executebuiltin("Container.Update(" + contexturl + ")")
 
 
 @site.register()
@@ -89,32 +160,41 @@ def Playvid(url, name, download=None):
 @site.register()
 def Search(url, keyword=None):
     if not keyword:
-        site.search_dir(url, 'Search')
+        site.search_dir(url, "Search")
     else:
-        List(url + keyword.replace(' ', '+'))
+        List(url + keyword.replace(" ", "+"))
 
 
 @site.register()
 def Related(url):
-    contexturl = (utils.addon_sys + "?mode=" + str('pmvhaven.List') + "&url=" + urllib_parse.quote_plus(url))
-    xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+    contexturl = utils.addon_sys + "?mode=pmvhaven.List&url=" + urllib_parse.quote_plus(url)
+    xbmc.executebuiltin("Container.Update(" + contexturl + ")")
 
 
 @site.register()
 def Lookupinfo(url):
-    tags, models = url.split('|')
-    tags = tags.split(',') if tags else []
-    models = models.split(',') if models else []
+    tags, models = url.split("|")
+    tags = tags.split(",") if tags else []
+    models = models.split(",") if models else []
 
     lookup_list = {}
     for tag in tags:
-        lookup_list["Tag - " + tag] = site.url + 'api/videos?limit=32&sort=-releaseDate&page=1&tagMode=OR&expandTags=false&tags={}'.format(tag)
+        lookup_list["Tag - " + tag] = (
+            site.url
+            + "api/videos?limit=32&sort=-releaseDate&page=1&tagMode=OR&expandTags=false&tags={}".format(
+                tag
+            )
+        )
 
     for model in models:
-        lookup_list["Model - " + model] = site.url + 'api/videos/search?limit=32&page=1&q={}'.format(model)
+        lookup_list["Model - " + model] = (
+            site.url + "api/videos/search?limit=32&page=1&q={}".format(model)
+        )
 
-    selected_item = utils.selector('Choose item', lookup_list, show_on_one=True)
+    selected_item = utils.selector("Choose item", lookup_list, show_on_one=True)
     if not selected_item:
         return
-    contexturl = (utils.addon_sys + "?mode=pmvhaven.List&url=" + urllib_parse.quote_plus(selected_item))
-    xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+    contexturl = (
+        utils.addon_sys + "?mode=pmvhaven.List&url=" + urllib_parse.quote_plus(selected_item)
+    )
+    xbmc.executebuiltin("Container.Update(" + contexturl + ")")
--- tests/sites/test_pmvhaven.py
+++ tests/sites/test_pmvhaven.py
@@ -1,165 +1,82 @@
-"""Tests for pmvhaven site implementation."""
+"""Tests for PMVHaven site implementation."""
 
 import json
-from unittest.mock import MagicMock
 
 from resources.lib.sites import pmvhaven
 
 
-def _sample_payload(has_next=True):
-    return {
+def test_format_duration_uses_duration_seconds_fallback():
+    """Fallback formatter converts seconds when the API omits duration."""
+    assert pmvhaven._format_duration({"durationSeconds": 38}) == "0:38"
+    assert pmvhaven._format_duration({"durationSeconds": 2182}) == "36:22"
+    assert pmvhaven._format_duration({"durationSeconds": 3723}) == "1:02:03"
+
+
+def test_list_handles_missing_duration(monkeypatch):
+    """List() should not fail when PMVHaven omits the duration field."""
+    payload = {
         "videos": [
             {
-                "title": "Clip One",
-                "thumbnailUrl": "https://pmvhaven.com/thumb1.jpg",
-                "videoUrl": "https://pmvhaven.com/video1.mp4",
-                "duration": "01:23",
+                "title": "Fallback Duration Video",
+                "thumbnailUrl": "https://video.pmvhaven.com/thumb.webp",
+                "videoUrl": "https://video.pmvhaven.com/video.mp4",
+                "durationSeconds": 38,
                 "height": 1080,
-                "hasExtremeContent": True,
-                "tags": ["tag1", "tag2"],
-                "starsTags": ["model1"],
-            },
-            {
-                "title": "Clip Two",
-                "thumbnailUrl": "https://pmvhaven.com/thumb2.jpg",
-                "hlsMasterPlaylistUrl": "https://pmvhaven.com/video2.m3u8",
-                "duration": "02:34",
-                "height": 720,
-                "tags": [],
-                "starsTags": [],
-            },
+                "tags": ["TagA", "TagB"],
+                "starsTags": ["ModelA"],
+            }
         ],
-        "pagination": {"hasNext": has_next, "page": 1},
+        "pagination": {"hasNext": False},
     }
 
-
-def test_main_menu(monkeypatch):
-    dirs = []
-    monkeypatch.setattr(
-        pmvhaven.site,
-        "add_dir",
-        lambda name, url, mode, iconimage=None, **kwargs: dirs.append((name, url, mode)),
-    )
-    monkeypatch.setattr(pmvhaven, "List", lambda *a, **k: None)
-    monkeypatch.setattr(pmvhaven.utils, "eod", lambda: None)
-
-    pmvhaven.Main()
-
-    assert dirs == [
-        (
-            "[COLOR hotpink]Search[/COLOR]",
-            "https://pmvhaven.com/api/videos/search?limit=32&page=1&q=",
-            "Search",
-        )
-    ]
-
-
-def test_list_parses_json_and_adds_next_page(monkeypatch):
-    payload = _sample_payload(has_next=True)
     downloads = []
-    dirs = []
 
-    monkeypatch.setattr(pmvhaven.utils, "getHtml", lambda *a, **k: json.dumps(payload))
+    monkeypatch.setattr(pmvhaven.utils, "getHtml", lambda url, referer=None: json.dumps(payload))
+    monkeypatch.setattr(pmvhaven.utils, "eod", lambda: None)
     monkeypatch.setattr(
         pmvhaven.site,
         "add_download_link",
-        lambda name, url, mode, iconimage, desc, **kwargs: downloads.append(
-            {"name": name, "url": url, "mode": mode, "quality": kwargs.get("quality")}
+        lambda name, url, mode, icon, desc="", **kw: downloads.append(
+            {
+                "name": name,
+                "url": url,
+                "icon": icon,
+                "duration": kw.get("duration"),
+                "quality": kw.get("quality"),
+            }
         ),
     )
-    monkeypatch.setattr(
-        pmvhaven.site,
-        "add_dir",
-        lambda name, url, mode, iconimage=None, **kwargs: dirs.append((name, url, mode, kwargs.get("contextm"))),
-    )
-    monkeypatch.setattr(pmvhaven.utils, "eod", lambda: None)
 
-    pmvhaven.List("https://pmvhaven.com/api/videos?limit=32&page=1")
+    pmvhaven.List("https://pmvhaven.com/api/videos?page=1")
 
-    assert downloads[0]["name"].endswith("[COLOR red] EXTREME[/COLOR]")
-    assert downloads[0]["url"] == "https://pmvhaven.com/video1.mp4"
+    assert len(downloads) == 1
+    assert downloads[0]["name"] == "Fallback Duration Video"
+    assert downloads[0]["url"] == "https://video.pmvhaven.com/video.mp4"
+    assert downloads[0]["icon"] == "https://video.pmvhaven.com/thumb.webp"
+    assert downloads[0]["duration"] == "0:38"
     assert downloads[0]["quality"] == "1080p"
-    assert downloads[1]["url"] == "https://pmvhaven.com/video2.m3u8"
-    assert dirs[0][0] == "Next Page (2)"
-    assert "pmvhaven.GotoPage" in dirs[0][3][0][1]
 
 
-def test_list_handles_missing_pagination(monkeypatch):
-    payload = {"videos": []}
-    dirs = []
+def test_list_skips_entries_without_title_or_stream(monkeypatch):
+    """List() should skip incomplete entries instead of raising exceptions."""
+    payload = {
+        "videos": [
+            {"title": "Missing Stream", "thumbnailUrl": "thumb", "durationSeconds": 10},
+            {"videoUrl": "https://video.pmvhaven.com/video.mp4", "thumbnailUrl": "thumb"},
+        ],
+        "pagination": {"hasNext": False},
+    }
 
-    monkeypatch.setattr(pmvhaven.utils, "getHtml", lambda *a, **k: json.dumps(payload))
+    downloads = []
+
+    monkeypatch.setattr(pmvhaven.utils, "getHtml", lambda url, referer=None: json.dumps(payload))
+    monkeypatch.setattr(pmvhaven.utils, "eod", lambda: None)
     monkeypatch.setattr(
         pmvhaven.site,
-        "add_dir",
-        lambda name, url, mode, iconimage=None, **kwargs: dirs.append((name, url, mode)),
-    )
-    monkeypatch.setattr(pmvhaven.utils, "eod", lambda: None)
-
-    pmvhaven.List("https://pmvhaven.com/api/videos?limit=32&page=1")
-
-    assert dirs == []
-
-
-def test_playvid_plays_direct_link(monkeypatch):
-    played = []
-
-    class MockPlayer:
-        def __init__(self, *a, **k):
-            self.progress = MagicMock()
-
-        def play_from_direct_link(self, url):
-            played.append(url)
-
-    monkeypatch.setattr(pmvhaven.utils, "VideoPlayer", MockPlayer)
-
-    pmvhaven.Playvid("https://pmvhaven.com/video1.mp4", "Name")
-
-    assert played == ["https://pmvhaven.com/video1.mp4"]
-
-
-def test_search(monkeypatch):
-    search_dir = []
-    list_calls = []
-    monkeypatch.setattr(pmvhaven.site, "search_dir", lambda *a, **k: search_dir.append((a, k)))
-    monkeypatch.setattr(pmvhaven, "List", lambda url: list_calls.append(url))
-
-    pmvhaven.Search("https://pmvhaven.com/api/videos/search?limit=32&page=1&q=")
-    pmvhaven.Search(
-        "https://pmvhaven.com/api/videos/search?limit=32&page=1&q=",
-        keyword="abc def",
+        "add_download_link",
+        lambda *args, **kwargs: downloads.append((args, kwargs)),
     )
 
-    assert search_dir
-    assert list_calls == ["https://pmvhaven.com/api/videos/search?limit=32&page=1&q=abc+def"]
+    pmvhaven.List("https://pmvhaven.com/api/videos?page=1")
 
-
-def test_goto_page_updates_container(monkeypatch):
-    monkeypatch.setattr(pmvhaven.xbmcgui, "Dialog", lambda: type("D", (), {"numeric": lambda *a: "7"})())
-    executed = []
-    monkeypatch.setattr(pmvhaven.xbmc, "executebuiltin", lambda cmd: executed.append(cmd))
-
-    pmvhaven.GotoPage("https://pmvhaven.com/api/videos?limit=32&page=2")
-
-    assert executed
-    assert "pmvhaven.List" in executed[0]
-    assert "page%3D7" in executed[0]
-
-
-def test_lookupinfo_builds_selector_options(monkeypatch):
-    selected = []
-    executed = []
-
-    monkeypatch.setattr(
-        pmvhaven.utils,
-        "selector",
-        lambda title, lookup_list, show_on_one=False: selected.append(lookup_list) or next(iter(lookup_list.values())),
-    )
-    monkeypatch.setattr(pmvhaven.xbmc, "executebuiltin", lambda cmd: executed.append(cmd))
-
-    pmvhaven.Lookupinfo("tag1,tag2|model1")
-
-    assert selected
-    assert "Tag - tag1" in selected[0]
-    assert "Model - model1" in selected[0]
-    assert executed
+    assert downloads == []
