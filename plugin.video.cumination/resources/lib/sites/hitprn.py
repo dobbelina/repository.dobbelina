f63e38a50e6e4de47049e72b674e1fdb5b1a4f87
dobbelina <46063764+dobbelina@users.noreply.github.com>
2026-06-06 08:06:00 +0000

    hitprn fixes #1878

--- plugin.video.cumination/resources/lib/sites/hitprn.py
+++ plugin.video.cumination/resources/lib/sites/hitprn.py
@@ -16,55 +16,69 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import re
+import xbmc
+import xbmcgui
 from six.moves import urllib_parse
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('hitprn', '[COLOR hotpink]Hitprn[/COLOR]', 'https://www.hitprn.com/', 'https://www.hitprn.com/wp-content/uploads/2021/03/hitprn-logo.png', 'hitprn')
+site = AdultSite('hitprn', '[COLOR hotpink]Hitprn[/COLOR]', 'https://www.hitprn.net/', 'https://hitprn.net/wp-content/uploads/2025/02/hitprn-logo.png', 'hitprn')
 
 addon = utils.addon
 
 
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Sites[/COLOR]', site.url, 'Sites', '', '')
-    site.add_dir('[COLOR hotpink]Most Views[/COLOR]', site.url + 'page/1/?orderby=views', 'List', site.img_cat)
-    site.add_dir('[COLOR hotpink]Likes[/COLOR]', site.url + 'page/1/?orderby=likes', 'List', site.img_cat)
     site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + '?s=', 'Search', site.img_search)
-    List(site.url + 'page/1/?orderby=date')
+    List(site.url + 'page/1/?filter=latest')
 
 
 @site.register()
 def List(url):
     listhtml = utils.getHtml(url)
-    match = re.compile(r'class="clip-link" data-id="\d+" title="([^"]+)" href="([^"]+)".*?src="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(listhtml)
-    for name, videopage, img in match:
-        name = utils.cleantext(name)
+    if 'It looks like nothing was found for this search' in listhtml:
+        utils.notify(msg='No videos found!')
+        return
 
-        contexturl = (utils.addon_sys
-                      + "?mode=hitprn.Lookupinfo"
-                      + "&url=" + urllib_parse.quote_plus(videopage))
-        contextmenu = [('[COLOR deeppink]Lookup info[/COLOR]', 'RunPlugin(' + contexturl + ')')]
+    delimiter = r'<article data-video-id="video'
+    re_videopage = '<a href="([^"]+)"'
+    re_name = 'title="([^"]+)"'
+    re_img = r'src="([^"]+)"'
+    re_duration = r'fa-clock-o"></i>\D*([^<]+)</span>'
+    skip = 'Terms of Use'
 
-        site.add_download_link(name, videopage, 'Play', img, name, contextm=contextmenu)
+    cm = []
+    cm_lookupinfo = (utils.addon_sys + "?mode=" + str('hitprn.Lookupinfo') + "&url=")
+    cm.append(('[COLOR deeppink]Lookup info[/COLOR]', 'RunPlugin(' + cm_lookupinfo + ')'))
+    cm_related = (utils.addon_sys + "?mode=" + str('hitprn.Related') + "&url=")
+    cm.append(('[COLOR deeppink]Related videos[/COLOR]', 'RunPlugin(' + cm_related + ')'))
 
-    np = re.compile('Next page" href="([^"]+)"', re.DOTALL | re.IGNORECASE).search(listhtml)
-    if np:
-        site.add_dir('Next Page... ({0})'.format(np.group(1).split('/')[-2]), np.group(1), 'List', site.img_next)
+    utils.videos_list(site, 'hitprn.Playvid', listhtml, delimiter, re_videopage, re_name, re_img, re_duration=re_duration, skip=skip, contextm=cm)
+
+    re_npurl = r'class="current">[^=]+?href="([^"]+)"'
+    re_npnr = r'class="current">.+?class="inactive">(\d+)<'
+    re_lpnr = r'/page/(\d+)/[^"]*">Last<'
+    utils.next_page(site, 'hitprn.List', listhtml, re_npurl, re_npnr, re_lpnr=re_lpnr, contextm='hitprn.GotoPage')
     utils.eod()
 
 
 @site.register()
-def Sites(url):
-    siteshtml = utils.getHtml(url, site.url)
-    match = re.compile(r'class="level-\d+" value="([^"]+)">([^<]+)</option', re.DOTALL | re.IGNORECASE).findall(siteshtml)
-    for sitepage, name in match:
-        name = name.replace('&nbsp;&nbsp;&nbsp;', '- ')
-        name = utils.cleantext(name)
-        siteurl = site.url + '?cat=' + sitepage
-        site.add_dir(name, siteurl, 'List')
-    utils.eod()
+def GotoPage(list_mode, url, np, lp):
+    dialog = xbmcgui.Dialog()
+    pg = dialog.numeric(0, 'Enter Page number')
+    if pg:
+        url = url.replace('/page/{}/'.format(np), '/page/{}/'.format(pg))
+        if int(lp) > 0 and int(pg) > int(lp):
+            utils.notify(msg='Out of range!')
+            return
+        contexturl = (utils.addon_sys + "?mode=" + str(list_mode) + "&url=" + urllib_parse.quote_plus(url))
+        xbmc.executebuiltin('Container.Update(' + contexturl + ')')
+
+
+@site.register()
+def Related(url):
+    contexturl = (utils.addon_sys + "?mode=" + str('hitprn.List') + "&url=" + urllib_parse.quote_plus(url))
+    xbmc.executebuiltin('Container.Update(' + contexturl + ')')
 
 
 @site.register()
@@ -72,32 +86,23 @@ def Search(url, keyword=None):
     if not keyword:
         site.search_dir(url, 'Search')
     else:
-        url += keyword.replace(' ', '+') + '&orderby=date'
-        List(url)
+        List(url + keyword.replace(' ', '+'))
 
 
 @site.register()
-def Play(url, name, download=None):
+def Playvid(url, name, download=None):
     vp = utils.VideoPlayer(name, download)
     vp.progress.update(25, "[CR]Loading video page[CR]")
+
     html = utils.getHtml(url, site.url)
-    sources = re.compile(r'''<source\s*src="([^"]+)".+?label="([^"]+)''', re.DOTALL | re.IGNORECASE).findall(html)
-    sources = {quality: videourl for videourl, quality in sources}
-    videourl = utils.selector('Select quality', sources, setting_valid='qualityask', sort_by=lambda x: int(x[:-1]), reverse=True)
-    if videourl:
-        vp.play_from_direct_link(videourl + '|Referer=' + site.url)
-    else:
-        vp.progress.close()
-        return
+    vp.play_from_html(html, url)
 
 
 @site.register()
 def Lookupinfo(url):
     lookup_list = [
-        ("Site", r'/(sites/[^"]+)"\s+rel="category tag">([^<]+)<', ''),
-        ("Pornstar", r'/(pornstar/[^"]+)"\s+rel="tag">([^<]+)<', ''),
-        ("Tag", r'/(tag/[^"]+)"\s+rel="tag">([^<]+)<', '')
+        ("Pornstar", r'/(actor/[^"]+)" class="label" title="([^"]+)"', ''),
+        ("Tag", r'/(tag/[^"]+)" class="label" title="([^"]+)"', '')
     ]
-
     lookupinfo = utils.LookupInfo(site.url, url, 'hitprn.List', lookup_list)
     lookupinfo.getinfo()
