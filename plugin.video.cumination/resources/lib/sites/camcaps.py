892a905bf62456dcd8ff9b852ed9f5f6bb065172
camilt <33828321+camilt@users.noreply.github.com>
2026-09-07 23:27:42 +0300

    Camcaps small fix player

--- plugin.video.cumination/resources/lib/sites/camcaps.py
+++ plugin.video.cumination/resources/lib/sites/camcaps.py
@@ -102,8 +102,11 @@ def Play(url, name, download=None):
         if '/vtplayer.net/' in refurl:
             refurl = refurl.replace('embed-', '')
         if vp.resolveurl.HostedMediaFile(refurl):
-            vp.play_from_link_to_resolve(refurl)
-            return
+            try:
+                vp.play_from_link_to_resolve(refurl)
+                return
+            except Exception as e:
+                utils.kodilog(f"Resolve failed: {e}")
         refpage = utils.getHtml(refurl)
         if '/playerz/' in refurl:
             videourl = re.compile(r'"src":"\.([^"]+)"', re.DOTALL | re.IGNORECASE).findall(refpage)[0]
